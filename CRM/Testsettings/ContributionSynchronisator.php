5c409ba039a877a5ee81898e7c7a93c33f891308
Jaap Jansma <info@edeveloper.nl>
2014-12-12 14:03:13 +0100

    added initial test settings for the sp

---
+++ CRM/Testsettings/ContributionSynchronisator.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This class prevents contribution to sync to Odoo
+ */
+class CRM_Testsettings_ContributionSynchronisator extends CRM_OdooContributionSync_ContributionSynchronisator {
+  
+  public function isThisItemSyncable(CRM_Odoosync_Model_OdooEntity $sync_entity) {
+    $return = parent::isThisItemSyncable($sync_entity);
+    //do not sync contributions with a date before 13 december 2014
+    if ($return) {
+      $contribution = $this->getContribution($sync_entity->getEntityId());
+      $date = new DateTime($contribution['receive_date']);
+      if ($date->format('Ymd') < 20141213) {
+        $return = false;
+      }
+    }
+    
+    //to test we return false so no contributions are synced to Odoo
+    return false;
+    
+    //if the contact test is succesfull we enable normal sync
+    //return $return;
+  }
+  
+}
+
